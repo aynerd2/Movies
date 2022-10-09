15b4327b6fce35774ce5b4812e30598d7ddc31c6
aynerd <olawaleayowande@gmail.com>
2022-10-09 11:08:41 +0100

    Simple Quote Application with JavaScript

--- index.js
+++ index.js
@@ -142,5 +142,73 @@
 
 
 
+// const revealbutton = document.querySelector('.revealbtn');
+// const hiddenContent = document.querySelector('.hiddencontent');
 
+// function contentShow (){
+
+//       if (hiddenContent.classList.contains('revealbtn')){
+//             hiddenContent.classList.remove('revealbtn')
+//       }else{
+//             hiddenContent.classList.add('revealbtn')
+//       }
+// }
+
+// revealbutton.addEventListener("click", contentShow);
+
+
+
+// QUOTE PROJECT
+
+
+let btn = document.querySelector('#new-quote');
+let quote = document.querySelector('.quote');
+let person = document.querySelector('.person');
+
+const quotes = 
+[
+      
+            {
+                  quote: `"The best way to find yourself is to lose yourself in the service of others. "`,
+                  person: `Mahatma Gandhi`
+            },
+
+            {
+                  quote: `"If you want to live a happy life, tie it to a goal, not to people or things. "`,
+                  person: `Albert Einstein`
+            },
+
+
+            {
+                  quote: `"At his best, man is the nooblest of all animals; separated from law and justice he is the worst. "`,
+                  person: `Aristotle`
+            },
+
+
+            {
+                  quote: `"Your time is limited, so dont waste it living someone elses life."`,
+                  person: `Steve Jobs`
+            },
+
+            {
+                  quote: `"Tell me and i forget. Teach me and I remember. Involve me and i learn."`,
+                  person: `Benjamin Franklin`
+            },
+
+            {
+                  quote: `"It does not matter how slowly you go as long as you do not stop"`,
+                  person: `Confucius`
+            },
+
+
+];
+
+
+
+btn.addEventListener('click', function(){
+
+      let random = Math.floor(Math.random() * quotes.length);
+      quote.innerText = quotes[random].quote;
+      person.innerText = quotes[random].person;
+})
 
